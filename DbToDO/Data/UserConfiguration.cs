9a49e71090531d9e4a39bf0fb274e3e04054e58e
Volodymyr Matiishchuk <Volodymyr.matiishchuk@gmail.com>
2021-10-21 14:58:32 +0300

    Add UserConfiguration

---
+++ DbToDO/Data/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDo.Models;
+
+namespace DbToDo.Data
+{
+    class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(k => k.UsedId);
+            builder.Property(n => n.Name).IsRequired().HasMaxLength(30);
+        }
+    }
+}
