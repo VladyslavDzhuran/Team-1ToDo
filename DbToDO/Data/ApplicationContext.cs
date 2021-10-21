f7d8e51e2612959750252715b2eb576ee27a92cd
Volodymyr Matiishchuk <Volodymyr.matiishchuk@gmail.com>
2021-10-21 15:12:18 +0300

    Add modelBuilder  for UserConfiguration

---
+++ DbToDO/Data/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using DbToDo.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDo.Models;
+
+namespace DbToDo
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
